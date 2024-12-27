ef7f9a2a11f4677281c4dd5fc8a774547fad06c8
eugene.levenetc <eugene.levenetc@jetbrains.com>
2024-12-27 15:58:26 +0100

    Add SelectionTextOverlay

---
+++ src/p5000/text/SelectionTextOverlay.ts
@@ -0,0 +1,81 @@
+import TextOverlay from "./TextOverlay";
+import TextView from "../TextView";
+import p5 from "p5";
+import {Backspace, Escape, KeyboardHandler, KeyChar} from "../keyboard/KeyboardHandler";
+import {dropLastChar} from "../utils/stringUtils";
+
+class SelectionTextOverlay implements TextOverlay {
+
+    selection: string = ""
+
+    constructor(keyboardHandler: KeyboardHandler) {
+        keyboardHandler.addListener(keyValue => {
+            if (keyValue instanceof Backspace) {
+                this.selection = dropLastChar(this.selection)
+            } else if (keyValue instanceof Escape) {
+                this.selection = ""
+            } else if (keyValue instanceof KeyChar) {
+                this.selection += keyValue.char
+            }
+        })
+    }
+
+    render(view: TextView, p: p5): void {
+        const intersections = getIntersections(view.title, this.selection)
+        if (intersections.length > 0) {
+            console.log("found intersections: " + intersections.length)
+        }
+        intersections.forEach(intersection => {
+            const prefix = intersection.prefix
+            const postfix = intersection.postfix
+            const match = intersection.match
+            const textHeight = view.getHeight(p);
+            const padding = textHeight / 10;
+            const x = view.x + p.textWidth(prefix) - padding
+            const y = view.y + padding
+            const width = p.textWidth(match) + padding * 2
+
+            const height = textHeight
+            console.log("w(" + match + "): " + width)
+            p.push()
+            p.blendMode("difference")
+            p.fill(255, 255, 255)
+            p.rect(x, y, width, height)
+            p.pop()
+        })
+    }
+}
+
+function getIntersections(str: string, search: string): SearchGroup[] {
+
+    if (search.length > str.length) return []
+    if (search.length == 0) return []
+    if (str.length == 0) return []
+
+    const result: SearchGroup[] = [];
+    let startIndex = 0;
+
+    while (startIndex < str.length) {
+        const index = str.toLowerCase().indexOf(search.toLowerCase(), startIndex);
+        if (index === -1) break;
+
+        const prefix = str.substring(0, index);
+        const match = search;
+        const postfix = str.substring(index + search.length);
+
+        result.push({prefix, match, postfix});
+
+        // Move to the next character after the current match
+        startIndex = index + 1;
+    }
+
+    return result;
+}
+
+class SearchGroup {
+    prefix: string
+    match: string
+    postfix: string
+}
+
+export default SelectionTextOverlay
---
+++ src/p5000/text/TextOverlay.ts
@@ -0,0 +1,8 @@
+import TextView from "../TextView";
+import p5 from "p5";
+
+interface TextOverlay {
+    render(view: TextView, p: p5)
+}
+
+export default TextOverlay
