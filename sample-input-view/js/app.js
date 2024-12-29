2a9edc8ed74e7c2f14286621ecbd19c56f73f24f
eugene.levenetc <eugene.levenetc@jetbrains.com>
2024-12-29 23:31:38 +0100

    Add InputView sample

---
+++ sample-input-view/js/app.js
@@ -0,0 +1,39 @@
+import p5 from "p5";
+import {Free} from "../../src/p5000/containers/Free";
+import {ColorDrawable} from "../../src/p5000/drawable/ColorDrawable";
+import {InputView} from "../../src/p5000/text/InputView";
+import {TextStyle} from "../../src/p5000/text/TextStyle";
+import Align from "../../src/p5000/Align";
+import {KeyboardHandlerImpl} from "../../src/p5000/keyboard/KeyboardHandler";
+
+const root = new Free()
+root.background = new ColorDrawable([50, 0, 0, 255])
+
+const inputView = new InputView(
+    "Type something",
+    "",
+    new KeyboardHandlerImpl(),
+    new TextStyle(33, [255, 0, 0])
+)
+
+inputView.align = Align.CENTER
+
+root.addChild(inputView)
+
+function setup(p) {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.textSize(32);
+}
+
+function draw(p) {
+    p.background(0, 0, 0);
+    root.render(p)
+}
+
+const sketch = (p) => {
+    p.setup = () => setup(p);
+    p.draw = () => draw(p);
+    p.windowResized = () => p.resizeCanvas(p.windowWidth, p.windowHeight);
+};
+
+new p5(sketch);
