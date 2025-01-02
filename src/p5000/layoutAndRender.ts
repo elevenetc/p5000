6097a91087bcac357421ddc631a4e66654e587d0
eugene.levenetc <eugene.levenetc@jetbrains.com>
2025-01-02 15:09:10 +0100

    Add centralized hover handling

--- src/p5000/layoutAndRender.ts
+++ src/p5000/layoutAndRender.ts
@@ -1,9 +1,31 @@
 import View from "./View";
 import p5 from "p5";
 
-function layoutAndRender(view: View, p: p5): void {
+function layoutAndRender(
+    view: View,
+    p: p5,
+    onHover?: (view: View, p: p5) => void,
+    offHover?: (view: View, p: p5) => void
+): void {
     view.layout(p)
     view.render(p)
+    handleHover(view, p, onHover, offHover)
+}
+
+function handleHover(
+    root: View,
+    p: p5,
+    onHover?: (view: View, p: p5) => void,
+    offHover?: (view: View, p: p5) => void
+): void {
+    if (!onHover && !offHover) return
+    if (root.handleHover(p.mouseX, p.mouseY, p)) {
+        p.cursor('pointer');
+        onHover?.(root, p)
+    } else {
+        p.cursor('default')
+        offHover?.(root, p)
+    }
 }
 
 export {
