dfa6b33f409f84f3f829c994531fcfa3c50adb3e
eugene.levenetc <eugene.levenetc@jetbrains.com>
2025-01-30 21:31:32 +0100

    Add basic tree selected node following

--- sample-basic-tree/js/app.js
+++ sample-basic-tree/js/app.js
@@ -10,6 +10,8 @@ import {initP5000} from "../../src/p5000/initP5000";
 import FpsView from "../../src/p5000/debug/FpsView";
 import {getNumberOrDefault, storeNumber} from "../../src/p5000/cookies/storeValue";
 
+const follow = false
+
 const root = new Free()
 const tree = new BasicTreeView()
 root.addChild(tree)
@@ -19,8 +21,16 @@ let navigationView = new NavigationView();
 
 playbackView.background = new ColorDrawable([255, 0, 0])
 root.addChild(playbackView, Align.CENTER_BOTTOM)
-let controller = new PlaybackController(playbackView, (frame) => {
+let controller = new PlaybackController(playbackView, 50, (frame) => {
     tree.setSelectedNode(frame.id)
+
+    if (follow) {
+        let node = tree.getViewNode(frame.id)
+        if (node != null) {
+            tree.setTranslationX(node.x * -1, true)
+            tree.setTranslationY(node.y * -1, true)
+        }
+    }
 })
 
 let fpsView = new FpsView()
@@ -31,13 +41,13 @@ navigationView.setClickHandler((direction) => {
     let xDiff = p.width / 10
     let yDiff = p.height / 10
     if (direction === Direction.Left) {
-        tree.translateX(-xDiff)
-    } else if (direction === Direction.Right) {
         tree.translateX(xDiff)
+    } else if (direction === Direction.Right) {
+        tree.translateX(-xDiff)
     } else if (direction === Direction.Up) {
-        tree.translateY(-yDiff)
-    } else if (direction === Direction.Down) {
         tree.translateY(yDiff)
+    } else if (direction === Direction.Down) {
+        tree.translateY(-yDiff)
     }
     storeNumber("transX", tree.getTranslationX())
     storeNumber("transY", tree.getTranslationY())
