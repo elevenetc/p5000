6296ec9ddc9e054b5df54e7ec29d1bca3661aa46
eugene.levenetc <eugene.levenetc@jetbrains.com>
2025-05-05 19:21:35 +0200

    Add webgl mode

--- src/p5000/initP5000.ts
+++ src/p5000/initP5000.ts
@@ -2,9 +2,17 @@ import {layoutAndRender} from "./layoutAndRender";
 import View from "./View";
 import p5 from "p5";
 
-function initP5000(root: View): p5 {
+function initP5000(root: View, useWebGL: boolean = false): p5 {
     function setup(p) {
-        const canvas = p.createCanvas(p.windowWidth, p.windowHeight)
+        const canvas = p.createCanvas(p.windowWidth, p.windowHeight, useWebGL ? p.WEBGL : p.P2D)
+
+        if (useWebGL) {
+            console.log("USE WEBGL MODE")
+            root.setX(-p.windowWidth / 2)
+            root.setY(-p.windowHeight / 2)
+        }
+
+        p.disableFriendlyErrors = true
         p.textSize(32)
 
         canvas.mousePressed((event) => {
@@ -29,7 +37,14 @@ function initP5000(root: View): p5 {
     const sketch = (p) => {
         p.setup = () => setup(p);
         p.draw = () => draw(p);
-        p.windowResized = () => p.resizeCanvas(p.windowWidth, p.windowHeight);
+        p.windowResized = () => {
+            p.resizeCanvas(p.windowWidth, p.windowHeight);
+
+            if (useWebGL) {
+                root.setX(-p.windowWidth / 2)
+                root.setY(-p.windowHeight / 2)
+            }
+        };
     };
 
     return new p5(sketch)
