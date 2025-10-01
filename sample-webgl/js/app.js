303e23a775a0f2c48e8caaff44060db25cefe2aa
eugene.levenetc <eugene.levenetc@jetbrains.com>
2025-10-02 00:58:17 +0200

    Add basic rect rendering at playground-webgl

--- sample-webgl/js/app.js
+++ sample-webgl/js/app.js
@@ -1,14 +1,35 @@
 import {Free} from "../../src/p5000/containers/Free";
-import Align from "../../src/p5000/Align";
 import {initP5000} from "../../src/p5000/initP5000";
-//import {WebGLRect} from "../../src/p5000/views/WebGLRect";
+import {GlPlaygroundDrawable, GraphNode} from "./playground-webgl";
+import {DrawableView} from "../../src/p5000/views/DrawableView";
 import LogView from "../../src/p5000/debug/LogView";
-import {EmojiButton} from "../../src/p5000/button/EmojiButton";
+import Align from "../../src/p5000/Align";
+// import {playgroundWebGL} from "./playground-webgl";
+
+console.log("init app.js");
 
 const root = new Free()
 
-//root.addChild(new WebGLRect(), Align.CENTER)
-root.addChild(new EmojiButton("🤬"), Align.CENTER)
+//root.addChild(new EmojiButton("🤬"), Align.CENTER)
+//root.addChild(new LogView(), Align.LEFT_TOP)
+
+const rootNode = new GraphNode(300, 100, 600, 200, [255, 0, 0])
+rootNode.children.push(new GraphNode(1100, 100, 1600, 200, [0, 255, 0]))
+
+const glView = new DrawableView(
+    new GlPlaygroundDrawable(
+        rootNode
+    )
+)
+
+root.addChild(glView)
 root.addChild(new LogView(), Align.LEFT_TOP)
 
-initP5000(root, true)
+initP5000(
+    root,
+    true,
+    (p) => {
+        //
+    }
+)
+
